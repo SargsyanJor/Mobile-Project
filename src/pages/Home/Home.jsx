5850419a0fffe2fedc9c86cad9d72a192a4d783a
SargsyanJor <sargsyanjor322@gmail.com>
2025-11-05 19:18:55 +0400

    slider

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,56 @@
+import React, { useEffect, useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './Home.css';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+import { instance } from '../../App/App';
+
+
 export const Home = () => {
+
+
+  const [slider, setSlider] = useState([]);
+  useEffect(() => {
+    instance.get("/products")
+      .then((res) => setSlider(res.data))
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+
+        {
+          slider.map((elm) => (
+            <SwiperSlide>
+              <div>
+                <img src={elm.image} alt="" />
+                <li>{elm.title}</li>
+
+              </div>
+
+
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
+    </>
+  );
 }
